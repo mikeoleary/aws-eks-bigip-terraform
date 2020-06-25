9a6a91282679e03f91a31b4158e37a116382d520
Andrew Oteiza <oteizaa@gmail.com>
2020-06-25 00:20:05 -0300

    update AS3 version to 3.20

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -8,10 +8,10 @@ variable "cluster-name" {
 }
 
 variable "as3_rpm" {
-  default = "f5-appsvcs-3.15.0-6.noarch.rpm"
+  default = "f5-appsvcs-3.20.0-3.noarch.rpm"
 }
 variable "as3_rpm_url" {
-  default = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.15.0/f5-appsvcs-3.15.0-6.noarch.rpm"
+  default = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.20.0/f5-appsvcs-3.20.0-3.noarch.rpm"
 }
 
 variable "vpc_cidr_block" {
